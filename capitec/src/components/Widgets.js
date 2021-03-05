4708f5899783c5820dbae1a4fd660f7ec05678bf
nerdfleeks <mawayalebo@users.noreply.github.com>
2021-03-05 13:12:38 -0800

    creating widgets list component and adding it to global one component

--- capitec/src/components/Widgets.js
+++ capitec/src/components/Widgets.js
@@ -1,27 +1,34 @@
 import "./css/Widgets.css";
+import more_blue from "./assets/more_blue.svg";
+import { BrowserRouter, Link } from "react-router-dom";
 const Widgets = () => {
-    const widgets = [{id:1, name:"Easy Equities", description:"investing made easy", link:"", theme:"red"}];
+    const widgets = [{id:1, name:"EasyEquities", description:"investing made easy", link:"https://www.easyequities.co.za/", theme:"red", icon_link:"https://pbs.twimg.com/profile_images/1091315817537511424/6zKo2HJA_400x400.jpg"}];
     return ( 
         <div className="widgets">
+            <div className="widget_heading">
+                <span className="grey-text text-darken-2 widget_title">Widgets</span>
+            </div>
             {
                 widgets.map(widget =>(
-                    <div className="card widget_card" key={widget.id}>
-                        <div className="card-content widget_card_content">
-                            <div className="row ">
-                                <div className="widget_icon red col s2">
-                                    Icon
+                    <div className="card widget_card"  key={widget.id}>
+                        <a href={widget.link} target="_blank">
+                            <div className="card-content widget_card_content">
+                                <div className="widget_icon_box ">
+                                    <img src={widget.icon_link} alt="widget_icon"  className=""/>
                                 </div>
-                                <div className="title_description col s9">
-                                    <p className={widget.theme+"-text"}>{widget.name}</p>
-                                    <p>{widget.description}</p>
+                                <div className="title_description ">
+                                    <p className={widget.theme+"-text widget_name "}>{widget.name}</p>
+                                    <p className="">{widget.description}</p>
+                                </div>
+                                <div className="widget_more_icon right">
+                                    <img src={more_blue} alt="more icon" className="responsive-img right" width="5" height="5"/>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 ))
             }
         </div>
      );
 }
- 
 export default Widgets;
