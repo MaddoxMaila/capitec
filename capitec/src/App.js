edb77d33a063a5e1296b439fdada860f0118e272
nerdfleeks <mawayalebo@users.noreply.github.com>
2021-03-06 13:57:39 -0800

    moving Browser Router to App js

--- capitec/src/App.js
+++ capitec/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import SignedOut from './components/SignedOut';
 import GlobalOne from './components/GlobalOne';
+import { BrowserRouter as Router } from  "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     {/*<SignedOut/> */}
-      <GlobalOne/>
-    </div>
+    <Router>
+      <div className="App">
+       <GlobalOne/>
+      </div>
+    </Router>
+    
   );
 }
 
--- capitec/src/components/GlobalOne.js
+++ capitec/src/components/GlobalOne.js
@@ -1,21 +1,19 @@
 import "./css/GlobalOne.css";
 import Home from "./Home";
 import SignedOut from "./SignedOut";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
 
 const GlobalOne = () => {
     return (
         <div className="global_one">
-            <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        <Home/>
-                    </Route>
-                    <Route path="/sign" >
-                        <SignedOut/>
-                    </Route>
-                </Switch>
-            </Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+                <Route path="/sign" >
+                    <SignedOut/>
+                </Route>
+            </Switch>
         </div>
      );
 }
